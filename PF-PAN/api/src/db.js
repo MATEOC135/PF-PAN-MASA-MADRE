3b352bedc285cecc7410d54af51c61c7587e0258
gerardo londero <londerogerardo@gmail.com>
2023-07-26 21:04:36 -0300

    deploy master

--- PF-PAN/api/src/db.js
+++ PF-PAN/api/src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-//postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
+
 const {
 
   DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY,DB_PORT,DB_NAME
