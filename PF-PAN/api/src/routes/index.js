bdf32348cc588c7ea4539395eadd0b28bda393f8
DarioCordoneda <dario_cordoneda@hotmail.com>
2023-08-07 22:57:58 -0300

    actializacion backend

--- PF-PAN/api/src/routes/index.js
+++ PF-PAN/api/src/routes/index.js
@@ -8,6 +8,10 @@ const router = Router();
 
 router.use("/client",handlerClient)
 
+const stripeRoutes = require('../handlers/HandlerStripe');
+
+router.use('/stripe', stripeRoutes);
+
 // Configurar los routers  
 // Ejemplo: router.use('/auth', authRouter);
 
